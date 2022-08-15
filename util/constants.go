8141cca581c7e6a0caa94e02f316ae21fa2b478f
farmerrrrr <hansungff3846@gmail.com>
2022-08-15 20:02:00 +0900

    [add] Arguments 인덱스 상수 추가

--- util/constants.go
+++ util/constants.go
@@ -1,9 +1,16 @@
 package util
 
 const (
+	FunctionName = 0
+	Parameter = 1
+)
+
+const (
 	RegisterUser  = "registerUser"
 	GetPoint      = "getPoint"
 	TransferPoint = "transferPoint"
+)
 
+const (
 	SuccessMessage = "success"
 )
