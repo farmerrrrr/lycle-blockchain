3b24785c26c5798cd94d40276a7962ae577a931c
farmerrrrr <hansungff3846@gmail.com>
2022-08-15 19:54:48 +0900

    [fix] User의 필드 접근 범위 변경

--- reward/user.go
+++ reward/user.go
@@ -1,8 +1,8 @@
 package reward
 
 type User struct {
-	email        string `validate:"required,email"`
-	point        int    `validate:"required,gte=0"`
-	registeredAt string `validate:"required,rfc3339"`
-	updateAt     string `validate:"required,rfc3339"`
+	Email        string `validate:"required,email"`
+	Point        int    `validate:"required,gte=0"`
+	RegisteredAt string `validate:"required,rfc3339"`
+	UpdatedAt     string `validate:"required,rfc3339"`
 }
